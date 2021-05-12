fedf8c7f4065032de5bf32c826495441ebc49502
mdauthentic <muideen.lawal20@gmail.com>
2021-05-12 15:55:06 +0200

    data dump function added

--- app/services/datadrop.py
+++ app/services/datadrop.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List
 import logging
-# from rethinkdb import RethinkDB
+from rethinkdb import RethinkDB
 import urllib3
 import json
 from urllib3.exceptions import HTTPError
@@ -11,8 +11,7 @@ TBL_NAME = "lycees"
 
 
 def api_request() -> List[Dict[str, Any]]:
-    """Request data from REST API endpoints
-    """
+    """Request data from REST API endpoints"""
     http = urllib3.PoolManager()
     get_url = (
         "https://www.data.gouv.fr/fr/datasets/r/7a0d991f-23c0-4021-a23a-b0c7f051c51d"
@@ -21,7 +20,10 @@ def api_request() -> List[Dict[str, Any]]:
     logging.info(f"Attempting to get data {get_url}...")
     try:
         response = http.request(
-            "GET", get_url, headers={"User-Agent": "Mozilla/5.0"}, retries=urllib3.util.Retry(3)
+            "GET",
+            get_url,
+            headers={"User-Agent": "Mozilla/5.0"},
+            retries=urllib3.util.Retry(3),
         )
         data = json.loads(response.data.decode("utf8"))
     except HTTPError as err:
@@ -29,7 +31,15 @@ def api_request() -> List[Dict[str, Any]]:
     except urllib3.exceptions.MaxRetryError as err:
         logging.error(f"API unavailable at {get_url}", err)
 
-    results = [ data[i]["fields"] for i in range(len(data)) ]
+    results = [data[i]["fields"] for i in range(len(data))]
 
     return results
 
+
+def data_dump_db():
+    r = RethinkDB()
+    logging.info(f"Establishing database connection...")
+    with r.connect(db=DB_NAME) as conn:
+        json_result = api_request()
+        r.table(TBL_NAME).insert(json_result).run(conn)
+        logging.info(f"Data successfully imported into database")
