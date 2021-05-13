9b0d6bd7d1024510054a45f22aa6c27e9665b2f0
mdauthentic <muideen.lawal20@gmail.com>
2021-05-13 08:27:45 +0200

    database now read parameters from config file

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,15 @@
+import configparser
 from rethinkdb import RethinkDB
 
+config = configparser.ConfigParser()
+config.read("../db.ini")
+
 # rethink config
-RDB_HOST = "localhost"
-RDB_PORT = 28015
-LYCEES_DB = "test"
-TABLE_NAME_LYCEES = "lycees"
-TABLE_NAME_POSTAUX = "postaux"
+RDB_HOST = config["rethinkdb"]["host"]
+RDB_PORT = config["rethinkdb"]["port"]
+LYCEES_DB = config["rethinkdb"]["db"]
+TABLE_NAME_LYCEES = config["lycees"]["table"]
+TABLE_NAME_POSTAUX = config["postaux"]["table"]
 
 r = RethinkDB()
 connect_db = r.connect(host=RDB_HOST, port=RDB_PORT)
